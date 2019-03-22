7e08579ba751b20d9b8277d60b81835991815abd
bensonitguy <bensonitguy@gmail.com>
2019-03-22 12:34:16 +0530

    removed git access token

--- fetch-git.js
+++ fetch-git.js
@@ -4,6 +4,6 @@ const axios = require('axios');
 module.exports = axios.create({
 	baseURL: 'https://api.github.com',
 	headers: {
-		Authorization: 'token 50ffbfc0e9d6b7a95d80fa736fc93e15fcc1575f'
+		Authorization: ''
 	}
 });
