92e79d4c85290c47df46bbb7dce507976c0ef641
bensonitguy <bensonitguy@gmail.com>
2019-05-03 12:35:34 +0530

    updated config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 var config = {};
 
-config.projectId = 'pwaweathertest';
-config.keyRingId = 'benKeyRing'; // Name of the crypto key's key ring
-config.cryptoKeyId = 'benKey'; // Name of the crypto key, e.g. "my-key"
-config.ciphertextFileName = './Constants.js.encrypted';
-config.plaintextFileName = '/tmp/Constants.js';
+config.projectId = ''; // Name of your project
+config.keyRingId = ''; // Name of the crypto key's key ring
+config.cryptoKeyId = ''; // Name of the crypto key, e.g. "my-key"
+config.ciphertextFileName = './Constants.js.encrypted'; // encrypted file name
+config.plaintextFileName = '/tmp/Constants.js'; // plain text file to be written.
 
-config.filePath = '/tmp/';
-config.destination = '/admin/standard/';
+config.filePath = '/tmp/'; // temporary folder
+config.destination = '/admin/standard/'; // cloud storage destination folder
 config.decoded_file_content = "test"; //default file content
-config.bucketName = 'pwaweathertest.appspot.com';
+config.bucketName = ''; // Google Cloud Storage Bucket name
 
-config.repository_path = '/repos/bensonitguy/blogposts-with-react/contents/';
+config.repository_path = '/repos/{github-name}/{repo-name}/contents/'; // Github repo path to reterive the file
 
-config.file_list = ['package.json','src/index.js'];
+config.file_list = ['package.json','src/index.js']; // list of file to be upload if updated in github.
 
 module.exports = config;
